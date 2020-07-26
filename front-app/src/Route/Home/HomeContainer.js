e9759239b93eb684a975311dac02fb4a3bbfbb5e
eunjung <jenny.it.kr@gmail.com>
2020-07-26 16:17:22 +0900

    Test data transfer from container to presenter

--- front-app/src/Route/Home/HomeContainer.js
+++ front-app/src/Route/Home/HomeContainer.js
@@ -3,13 +3,50 @@ import HomePresenter from "./HomePresenter";
 
 export default class extends Component {
   state = {
-    test: "test",
+    campaign: null,
+    myOrganizations: null,
+    cards: null,
     loading: true,
     error: null,
   };
 
+  async componentDidMount() {
+    try {
+      let campaign, myOrganizations, cards;
+      const a = new Promise((resolve, reject) => {
+        setTimeout(
+          () =>
+            resolve({
+              campaign: "COVID 19",
+              myOrganizations: ["A", "B", "C"],
+              cards: ["라면", "물", "도서"],
+            }),
+          1000
+        );
+      });
+      await a.then((res) => {
+        campaign = res.campaign;
+        myOrganizations = res.myOrganizations;
+        cards = res.cards;
+      });
+      this.setState({ campaign, myOrganizations, cards });
+    } catch {
+      this.setState({ error: "error" });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
   render() {
-    const { test, loading, error } = this.state;
-    return <HomePresenter test={test} loading={loading} error={error} />;
+    const { campaign, myOrganizations, cards, loading, error } = this.state;
+    return (
+      <HomePresenter
+        campaign={campaign}
+        myOrganizations={myOrganizations}
+        cards={cards}
+        loading={loading}
+        error={error}
+      />
+    );
   }
 }
--- front-app/src/Route/Home/HomePresenter.js
+++ front-app/src/Route/Home/HomePresenter.js
@@ -1,3 +1,13 @@
 import React from "react";
 
-export default ({ test, loading, error }) => <h1>{test}</h1>;
+export default ({ campaign, myOrganizations, cards, loading, error }) =>
+  loading ? (
+    <h1>로딩중</h1>
+  ) : error ? (
+    <h1>에러발생</h1>
+  ) : (
+    <h1>
+      로딩끝 / {campaign} /{myOrganizations.map((organization) => organization)}{" "}
+      /{cards}
+    </h1>
+  );
