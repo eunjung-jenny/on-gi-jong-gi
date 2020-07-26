acb2a80b5b947c721d0519b89589e6460bc3c7f9
eunjung <jenny.it.kr@gmail.com>
2020-07-26 14:50:08 +0900

    Create Home route

--- front-app/src/App.js
+++ front-app/src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <h1>Poomeuro Start</h1>;
+  return (
+    <Router>
+      <Route path="/" exact={true} render={() => <h1>Home</h1>} />
+    </Router>
+  );
 }
-
-export default App;
