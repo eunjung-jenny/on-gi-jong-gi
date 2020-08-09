504731e9f1647728e416e6a5cccc8a4357480bf0
eunjung <jenny.it.kr@gmail.com>
2020-08-09 15:35:30 +0900

    json => js 로 변경

---
+++ front-app/src/data/feedback.js
@@ -0,0 +1,54 @@
+export default {
+  result: [
+    {
+      id: 1,
+      reward_id: 1,
+      content: "감사합니다 후원자님",
+    },
+    {
+      id: 2,
+      reward_id: 10,
+      content: "감사합니다 후원자님",
+    },
+    {
+      id: 3,
+      reward_id: 8,
+      content: "소중한 후원 소중히 사용하겠습니다",
+    },
+    {
+      id: 4,
+      reward_id: 10,
+      content: "감사합니다 후원자님",
+    },
+    {
+      id: 5,
+      reward_id: 6,
+      content: "사랑합니다",
+    },
+    {
+      id: 6,
+      reward_id: 8,
+      content: "감사합니다 후원자님",
+    },
+    {
+      id: 7,
+      reward_id: 10,
+      content: "감사합니다 후원자님",
+    },
+    {
+      id: 8,
+      reward_id: 2,
+      content: "좋은 인연이 이어지길 바랍니다.",
+    },
+    {
+      id: 9,
+      reward_id: 1,
+      content: "좋은 인연이 이어지길 바랍니다.",
+    },
+    {
+      id: 10,
+      reward_id: 6,
+      content: "사랑합니다",
+    },
+  ],
+};
