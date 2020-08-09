f1a3270190e81cd96ab86d8d8c57a9a6ca3a7947
eunjung <jenny.it.kr@gmail.com>
2020-08-09 23:33:01 +0900

    기관 상세 페이지 변경

--- front-app/src/Route/OrganizationDetail/OrganizationDetailContainer.js
+++ front-app/src/Route/OrganizationDetail/OrganizationDetailContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import OrganizationDetailPresenter from "./OrganizationDetailPresenter";
+import facility from "../../data/facility";
 
 export default class extends Component {
   state = {
@@ -12,21 +13,9 @@ export default class extends Component {
     try {
       let organization;
       const a = new Promise((resolve, reject) => {
-        setTimeout(
-          () =>
-            resolve({
-              organization: {
-                name: "A",
-                address: "서울시 광진구",
-                type: "아동양육시설",
-                liked: 137,
-                message: "늘 사랑을 나눠주셔서 감사합니다.",
-              },
-            }),
-          1000
-        );
+        setTimeout(() => resolve(facility.result[1]), 1000);
       });
-      await a.then((res) => (organization = res.organization));
+      await a.then((res) => (organization = res));
       this.setState({ organization });
     } catch {
       this.setState({ error: "error" });
--- front-app/src/Route/OrganizationDetail/OrganizationDetailPresenter.js
+++ front-app/src/Route/OrganizationDetail/OrganizationDetailPresenter.js
@@ -5,8 +5,53 @@ import Footer from "../../Component/Footer";
 
 const Container = styled.main`
   margin: 10px 20px;
-  border: 1px solid black;
   color: black;
+`;
+
+const Type = styled.div`
+  font-size: 14px;
+  color: gray;
+`;
+
+const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Col = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Name = styled.div`
+  font-size: 24px;
+  margin-right: 5px;
+`;
+
+const Description = styled.div`
+  margin: 10px 0;
+  padding: 10px;
+  background-color: #38d9a9;
+  border-radius: 10px;
+  color: white;
+`;
+
+const Address = styled.div`
+  margin-top: 4px;
+  font-size: 14px;
+  color: gray;
+`;
+
+const Liked = styled.div``;
+
+const Homepage = styled.div``;
+
+const Image = styled.img`
+  display: block;
+  margin: 0 auto;
+  width: 85vw;
+  height: 40vh;
 `;
 
 const OrganizationDetailPresenter = ({ organization, loading, error }) => (
@@ -17,9 +62,21 @@ const OrganizationDetailPresenter = ({ organization, loading, error }) => (
       "에러발생"
     ) : (
       <>
-        {organization.name} {organization.address} {organization.type}
-        {organization.liked}
-        {organization.message}
+        <Type>{organization.type}</Type>
+        <Row>
+          <Col>
+            <Name>{organization.name}</Name>
+            <Homepage href={organization.homepage}>
+              <i className="fas fa-home"></i>
+            </Homepage>
+          </Col>
+          <Liked>{organization.liked} ❤️</Liked>
+        </Row>
+        <Address>
+          {`${organization.address1} ${organization.address2} ${organization.address3}`}
+        </Address>
+        <Description>{organization.description[0]}</Description>
+        <Image src={organization.imgurl}></Image>
       </>
     )}
     <Footer />
